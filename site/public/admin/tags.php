7f28b1d43b1c1910badc4ffba360ed04833ef814
Geoffrey Goodman <ggoodman@badman.(none)>
2009-02-06 14:57:05 -0500

    Added missing files

---
+++ site/public/admin/tags.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '../../lib/bootstrap.php';
+
+function index_action($registry, $request, $response) {
+
+	foreach ($registry->dba->getDatabase()->select('article_tag')->column('tag_id')->where('tag_id LIKE ?', $request->q . '%') as $tag) {
+		echo $tag['tag_id']."\n";
+	}
+	exit();
+}
+
+?>
